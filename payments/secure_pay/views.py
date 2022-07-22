f385ec0f62cc248432163793957a65ce4c698a57
Marjun Makinano <marjunmakinanotwentytwenty@gmail.com>
2022-07-23 01:16:26 +0800

    Added secure_pay/pay

--- payments/secure_pay/views.py
+++ payments/secure_pay/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the secure pay index.")
+
+def pay(request):
+    return HttpResponse("/pay")
