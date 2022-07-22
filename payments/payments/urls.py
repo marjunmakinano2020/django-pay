8000940b381e33444a3def67e9961b57a55fb7ed
Marjun Makinano <marjunmakinanotwentytwenty@gmail.com>
2022-07-23 00:18:40 +0800

    Integrated secure_pay

--- payments/payments/urls.py
+++ payments/payments/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('secure_pay/', include('secure_pay.urls')),
 ]
