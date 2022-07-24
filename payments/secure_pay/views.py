3fd1f5c49ce5580361921e4f2845b2450504a9cb
Marjun Makinano <marjunmakinanotwentytwenty@gmail.com>
2022-07-24 16:51:56 +0800

    Updated views

--- payments/secure_pay/views.py
+++ payments/secure_pay/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from base64 import b64encode
 import requests
 import json
 
@@ -8,12 +9,18 @@ def index(request):
     return HttpResponse("Hello, world. You're at the secure pay index.")
 
 def pay(request):
+    client_id = '0oaxb9i8P9vQdXTsn3l5'
+    client_secret = '0aBsGU3x1bc-UIF_vDBA2JzjpCPHjoCP7oI6jisp'
+    basic_auth = client_id + ':' + client_secret
+    basic_auth64 = b64encode(basic_auth.encode("utf-8"))
+    basic_auth_string = 'Basic ' + basic_auth64.decode("utf-8")
+
     # Get access token
-    # Todo: Fix 'grant_type': 'client_credentials' and 'Authorization': 'Basic xxxxxxxx'
+    # Todo: Fix 'grant_type': 'client_credentials'
     # And auth=('0oaxb9i8P9vQdXTsn3l5', '0aBsGU3x1bc-UIF_vDBA2JzjpCPHjoCP7oI6jisp')
     url = 'https://welcome.api2.sandbox.auspost.com.au/oauth/token'
     data = {'grant_type': 'client_credentials', 'audience': 'https://api.payments.auspost.com.au'}
-    headers = {'Authorization': 'Basic xxxxxxxx', 'Content-Type': 'application/x-www-form-urlencoded'}
+    headers = {'Authorization': basic_auth_string, 'Content-Type': 'application/x-www-form-urlencoded'}
     r = requests.post(url, data=json.dumps(data), headers=headers, auth=('0oaxb9i8P9vQdXTsn3l5', '0aBsGU3x1bc-UIF_vDBA2JzjpCPHjoCP7oI6jisp'))
     # print(r.json())
 
