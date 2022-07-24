73c3ff911e7fffd5158ed556152b5eed4410e438
Marjun Makinano <marjunmakinanotwentytwenty@gmail.com>
2022-07-24 19:29:07 +0800

    Fixed merchantCode

--- payments/secure_pay/views.py
+++ payments/secure_pay/views.py
@@ -37,7 +37,7 @@ def pay(request):
     token = r["access_token"]
 
     # Create a payment
-    # Todo: Fix merchantCode, token, orderId, Idempotency-Key, Authorization, auth
+    # Todo: Fix token, orderId, Idempotency-Key, Authorization, auth
     payment_url = 'https://payments-stest.npe.auspost.zone/v2/payments'
     payment_data = {
         "amount": 10000,
