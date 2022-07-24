275ee3a5883ab47ac5678997b9245b71facc3dc2
Marjun Makinano <marjunmakinanotwentytwenty@gmail.com>
2022-07-24 16:57:37 +0800

    Fixed client_credentials

--- payments/secure_pay/views.py
+++ payments/secure_pay/views.py
@@ -16,7 +16,6 @@ def pay(request):
     basic_auth_string = 'Basic ' + basic_auth64.decode("utf-8")
 
     # Get access token
-    # Todo: Fix 'grant_type': 'client_credentials'
     # And auth=('0oaxb9i8P9vQdXTsn3l5', '0aBsGU3x1bc-UIF_vDBA2JzjpCPHjoCP7oI6jisp')
     url = 'https://welcome.api2.sandbox.auspost.com.au/oauth/token'
     data = {'grant_type': 'client_credentials', 'audience': 'https://api.payments.auspost.com.au'}
